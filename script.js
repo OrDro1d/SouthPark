bd94522679504aa64c04cec7e85a90c7b3354839
Osman Lyumanov <osmanlyumanov2005@gmail.com>
2024-09-23 23:08:19 +0300

    Попытка убить ̶у̶б̶и̶т̶ь̶ ̶К̶е̶н̶н̶и̶ воссоздать Южный Парк и его красоты

---
+++ script.js
@@ -0,0 +1,112 @@
+let step = 0;
+
+function draw(context) {
+	function sky(context) {
+		context.fillStyle = "rgb(175, 238, 238)";
+		context.beginPath();
+		context.moveTo(0, 0);
+		context.lineTo(1000, 0);
+		context.lineTo(1000, 500);
+		context.lineTo(0, 500);
+		context.closePath();
+		context.fill();
+	}
+
+	function background(context) {
+		// Горы
+		context.fillStyle = "rgb(60, 179, 113)";
+		context.beginPath();
+		context.moveTo(0, 360);
+		context.lineTo(500, 60);
+		context.lineTo(900, 360);
+		context.lineTo(0, 500);
+		context.closePath();
+		context.stroke();
+		context.fill();
+
+		context.fillStyle = "rgb(46, 139, 87)";
+		context.beginPath();
+		context.moveTo(500, 360);
+		context.lineTo(880, 60);
+		context.lineTo(1000, 200);
+		context.lineTo(1000, 360);
+		context.closePath();
+		context.stroke();
+		context.fill();
+
+		context.fillStyle = "rgb(34, 139, 34)";
+		context.beginPath();
+		context.moveTo(0, 160);
+		context.lineTo(200, 80);
+		context.lineTo(600, 360);
+		context.lineTo(0, 500);
+		context.closePath();
+		context.stroke();
+		context.fill();
+		// Земля
+		context.fillStyle = "rgb(245,245,245)";
+		context.beginPath();
+		context.moveTo(0, 360);
+		context.lineTo(1000, 360);
+		context.lineTo(1000, 500);
+		context.lineTo(0, 500);
+		context.closePath();
+		context.stroke();
+		context.fill();
+	}
+
+	function hill(context) {
+		context.fillStyle = "rgb(255,255,255)";
+		context.strokeStyle = "rgb(0,0,0)";
+		context.beginPath();
+		context.moveTo(0, 200);
+		context.lineTo(350, 380);
+		context.lineTo(700, 500);
+		context.lineTo(0, 500);
+		context.closePath();
+		context.stroke();
+		context.fill();
+	}
+
+	sky(context);
+	background(context);
+	hill(context);
+}
+
+// function move(points, dx, dy) {
+// 	for (let i = 0; i < points.length; i++) {
+// 		points[i].x += dx;
+// 		points[i].y += dy;
+// 	}
+// }
+
+// function scale(points, sx, sy) {
+// 	for (let i = 0; i < points.length; i++) {
+// 		points[i].x *= sx;
+// 		points[i].y *= sy;
+// 	}
+// }
+
+// function rotate(points, x, y, angle) {
+// 	for (let i = 0; i < points.length; i++) {
+// 		let newX = points[i].x * Math.sin(angle) + points[i].y * Math.cos(angle);
+// 		points[i].y = points[i].x * Math.cos(angle) - points[i].y * Math.sin(angle);
+// 		points[i].x = newX;
+// 	}
+// }
+
+let roll = document.querySelector("#roll");
+context = document.querySelector("#drawing").getContext("2d");
+
+draw(context);
+roll.addEventListener("click", () => {
+	step += 10;
+	draw(context);
+	context.fillStyle = "rgb(255,255,255)";
+	context.strokeStyle = "rgb(0,0,0)";
+	context.beginPath();
+	context.arc(60 + 4 * step, 260 + step, 20 + step, 0, 2 * Math.PI);
+	context.closePath();
+	context.stroke();
+	context.fill();
+});
